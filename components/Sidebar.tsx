855831d70115a79933c377f2389821ddbac2f4d6
Prathamesh Pimpalkar <91759972+Prathamesh0222@users.noreply.github.com>
2025-01-13 20:06:13 +0530

    Refactor Sidebar component for improved styling and add Dialog component for enhanced user interaction

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -49,9 +49,8 @@ const Sidebar = () => {
   return (
     <div className="flex min-h-screen">
       <div
-        className={`border-r transition-all duration-300 ease-in-out transform ${
-          expanded ? "translate-x-0 w-64" : "translate-x-0 w-18"
-        }`}
+        className={`border-r transition-all duration-300 ease-in-out transform ${expanded ? "translate-x-0 w-64" : "translate-x-0 w-18"
+          }`}
       >
         {expanded ? (
           <div className="flex flex-col justify-between h-full">
@@ -68,11 +67,10 @@ const Sidebar = () => {
                   <Link href={icon.link}>
                     <div
                       className={`flex items-center p-4 mt-4 rounded-xl mx-2
-                    ${
-                      pathname === icon.link
-                        ? "bg-blue-800 text-white"
-                        : "hover:bg-blue-800"
-                    }
+                    ${pathname === icon.link
+                          ? "bg-blue-800 text-white"
+                          : "hover:bg-blue-800"
+                        }
                     `}
                     >
                       {icon.icon}
@@ -81,29 +79,6 @@ const Sidebar = () => {
                   </Link>
                 </div>
               ))}
-            </div>
-            <div className="p-3 border-t flex gap-4 items-center">
-              <div>
-                <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                  <svg
-                    className="absolute w-12 h-12 text-gray-400 -left-1"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-              <div className="font-bold">
-                {session.data?.user.username}
-                <br />
-                {session.data?.user.email}
-              </div>
             </div>
           </div>
         ) : (
@@ -119,11 +94,10 @@ const Sidebar = () => {
               <div key={index}>
                 <Link
                   href={icon.link}
-                  className={`flex justify-end p-5 mt-4 ${
-                    pathname === icon.link
-                      ? "bg-blue-800 text-white rounded-xl"
-                      : "hover:bg-blue-800"
-                  }`}
+                  className={`flex justify-end p-5 mt-4 ${pathname === icon.link
+                    ? "bg-blue-800 text-white rounded-xl"
+                    : "hover:bg-blue-800"
+                    }`}
                 >
                   {icon.icon}
                 </Link>
