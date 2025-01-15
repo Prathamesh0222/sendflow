fc4652f0de62244a4a6b98c3e44d58d332b4f757
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-01-15 23:06:41 +0530

    Refactor Sidebar component for improved layout and icon updates
    - Replaced the "Home" icon with a "Dashboard" icon in the sidebar for better clarity.
    - Enhanced the sidebar layout to be responsive, ensuring it displays correctly on larger screens while maintaining functionality on smaller devices.
    - Updated the sidebar toggle functionality to improve user experience when expanding and collapsing the sidebar.
    - Adjusted styling for better visual consistency and accessibility across different screen sizes.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 import {
   ArrowLeftRight,
   Home,
+  LayoutDashboard,
   SquareChevronLeft,
   SquareChevronRight,
 } from "lucide-react";
-import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
@@ -19,8 +19,8 @@ interface IconProps {
 
 const Icons: IconProps[] = [
   {
-    name: "Home",
-    icon: <Home />,
+    name: "Dashboard",
+    icon: <LayoutDashboard />,
     link: "/dashboard/home",
   },
   {
@@ -31,7 +31,6 @@ const Icons: IconProps[] = [
 ];
 
 const Sidebar = () => {
-  const session = useSession();
   const pathname = usePathname();
   const [expanded, setExpanded] = useState(false);
 
@@ -40,17 +39,47 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="flex min-h-screen">
-      <div
-        className={`border-r transition-all duration-300 ease-in-out transform ${
-          expanded ? "translate-x-0 w-64" : "translate-x-0 w-18"
-        }`}
-      >
-        {expanded ? (
-          <div className="flex flex-col justify-between h-full">
+    <>
+      <div className="hidden lg:flex min-h-screen">
+        <div
+          className={`border-r hidden lg:block transition-all duration-300 ease-in-out transform ${
+            expanded ? "translate-x-0 w-64" : "translate-x-0 w-18"
+          }`}
+        >
+          {expanded ? (
+            <div className="flex flex-col justify-between h-full">
+              <div>
+                <div className="p-3 mt-2 flex">
+                  <SquareChevronLeft
+                    size={42}
+                    className="cursor-pointer hover:bg-blue-800 p-2 rounded-lg"
+                    onClick={toggleSidebar}
+                  />
+                </div>
+                {Icons.map((icon, index) => (
+                  <div key={index}>
+                    <Link href={icon.link}>
+                      <div
+                        className={`flex items-center p-4 mt-4 rounded-xl mx-2
+                    ${
+                      pathname === icon.link
+                        ? "bg-blue-800 text-white"
+                        : "hover:bg-blue-800"
+                    }
+                    `}
+                      >
+                        {icon.icon}
+                        <span className="ml-4">{icon.name}</span>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
             <div>
-              <div className="p-3 mt-2 flex">
-                <SquareChevronLeft
+              <div className="p-3 mt-2 flex justify-end">
+                <SquareChevronRight
                   size={42}
                   className="cursor-pointer hover:bg-blue-800 p-2 rounded-lg"
                   onClick={toggleSidebar}
@@ -58,51 +87,43 @@ const Sidebar = () => {
               </div>
               {Icons.map((icon, index) => (
                 <div key={index}>
-                  <Link href={icon.link}>
-                    <div
-                      className={`flex items-center p-4 mt-4 rounded-xl mx-2
-                    ${
+                  <Link
+                    href={icon.link}
+                    className={`flex justify-end p-5 mt-4 ${
                       pathname === icon.link
-                        ? "bg-blue-800 text-white"
+                        ? "bg-blue-800 text-white rounded-xl"
                         : "hover:bg-blue-800"
-                    }
-                    `}
-                    >
-                      {icon.icon}
-                      <span className="ml-4">{icon.name}</span>
-                    </div>
+                    }`}
+                  >
+                    {icon.icon}
                   </Link>
                 </div>
               ))}
             </div>
-          </div>
-        ) : (
-          <div>
-            <div className="p-3 mt-2 flex justify-end">
-              <SquareChevronRight
-                size={42}
-                className="cursor-pointer hover:bg-blue-800 p-2 rounded-lg"
-                onClick={toggleSidebar}
-              />
-            </div>
-            {Icons.map((icon, index) => (
-              <div key={index}>
-                <Link
-                  href={icon.link}
-                  className={`flex justify-end p-5 mt-4 ${
+          )}
+        </div>
+      </div>
+
+      <div className="lg:hidden fixed p-4 bottom-0 left-0 right-0 bg-background border-t">
+        <div className="flex gap-2 mx-24 justify-between">
+          {Icons.map((icon, index) => (
+            <div key={index}>
+              <Link href={icon.link}>
+                <div
+                  className={`flex gap-2 px-24  p-2.5 rounded-lg w-full ${
                     pathname === icon.link
-                      ? "bg-blue-800 text-white rounded-xl"
-                      : "hover:bg-blue-800"
+                      ? "bg-blue-800 text-white"
+                      : "bg-background hover:bg-blue-800"
                   }`}
                 >
-                  {icon.icon}
-                </Link>
-              </div>
-            ))}
-          </div>
-        )}
+                  {icon.icon} <span>{icon.name}</span>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
